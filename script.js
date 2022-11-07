0503c1493747193460cc3e45e0536121f995923c
Diogo Asenjo <diogoasenjo@outlook.com>
2022-11-07 19:31:27 -0300

    Teste de commit

--- script.js
+++ script.js
@@ -72,3 +72,5 @@ document.querySelector(`.again`).addEventListener(`click`, function () {
   document.querySelector(`body`).style.backgroundColor = `#222`;
   document.querySelector(`.number`).style.width = `15rem`;
 });
+
+//Teste de commit!
